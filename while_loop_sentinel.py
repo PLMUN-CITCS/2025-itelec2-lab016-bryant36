350a99099ae941c4c1df8d59b95b2a057db1a112
unknown <manaladbryantray_bsit@plmun.edu.ph>
2025-02-22 15:25:20 -0700

    Submitting Python Week 04 - Laboratory # 16

---
+++ while_loop_sentinel.py
@@ -0,0 +1,13 @@
+# BRYANT RAY M. MANALAD
+# ITELEC2
+# Laboratory #16 - Guided Coding Exercise: Basic Function Definition and calling
+
+
+def greet():
+    """Prints a greeting message."""
+
+    print("hello, Python Students!")
+
+
+greet()    
+    
